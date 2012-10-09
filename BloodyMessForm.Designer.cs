2e96f02e7dea5246d62d1c695c2aa476d0b94994
hbjoystick9@yahoo.com <hbjoystick9@yahoo.com@d287009f-08b5-18b3-02a1-90e1d481eaf3>
2012-10-09 09:42:55 +0000

    GUI Button event Fixed
    git-svn-id: http://bloodymess.googlecode.com/svn/trunk/BloodyMess@22 d287009f-08b5-18b3-02a1-90e1d481eaf3

--- BloodyMessForm.Designer.cs
+++ BloodyMessForm.Designer.cs
@@ -528,6 +528,8 @@ namespace BloodyMess
             this.button3.TabIndex = 10;
             this.button3.Text = "Check for Updates";
             this.button3.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button3_Click);
+            
             // 
             // BloodyMessForm
             // 
